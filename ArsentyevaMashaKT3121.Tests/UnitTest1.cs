3baf3b7dbf83f4003c869bb37f00c75263c265c6
79613 <79613@LAPTOP-F2EMFFGI>
2025-01-09 13:46:23 +0300

    lab6

---
+++ ArsentyevaMashaKT3121.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ArsentyevaMashaKT3121.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- ArsentyevaMashaKT3121/Program.cs
+++ ArsentyevaMashaKT3121/Program.cs
@@ -33,7 +33,7 @@ try
         app.UseSwaggerUI();
     }
 
-    app.UseMiddleware<ExceptionHandlerMiddleware>();
+//    app.UseMiddleware<ExceptionHandlerMiddleware>();
 
     app.UseAuthorization();
 
